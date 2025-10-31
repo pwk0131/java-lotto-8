025bd3044e8f27cbf8a189c6067a99fef79af61b
pwk0131 <pakr051323@gmail.com>
2025-10-31 23:07:44 +0900

    feat(lottos): 원시값 포장

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
+import java.util.List;
+import lotto.Lotto;
+
 public class Lottos {
+    private final List<Lotto> lottos;
+
+    public Lottos(List<Lotto> lottos) {
+        this.lottos = lottos;
+    }
+
+    // OutputView에서 출력을 담당할 toString
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Lotto lotto : lottos) {
+            sb.append(lotto.toString()).append("\n");
+        }
+        return sb.toString().trim(); // 마지막 줄바꿈 제거
+    }
 }
