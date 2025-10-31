5a463657d23025b6ed990378791f58739d14f906
pwk0131 <pakr051323@gmail.com>
2025-10-31 23:09:22 +0900

    feat(outputView): 구매 개수와 로또 목록 출력하는 기능 추가

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,9 +1,19 @@
 package lotto.view;
 
+import lotto.domain.Lottos;
+
 public class OutputView {
 
     public void printError(IllegalArgumentException e) {
         System.out.println(e.getMessage());
     }
-    
+
+    public void printTicketCount(int count) {
+        System.out.printf("\n%d개를 구매했습니다.\n", count);
+    }
+
+    public void printLottos(Lottos lottos) {
+        System.out.println(lottos.toString());
+    }
+
 }
