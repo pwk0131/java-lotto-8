81153ab2ba8f4acb0437238554da55baa02aa15f
pwk0131 <pakr051323@gmail.com>
2025-10-31 23:09:51 +0900

    feat(lottoGener): 로또 생성 기능 추가

--- src/main/java/lotto/service/RandomLottoGenerator.java
+++ src/main/java/lotto/service/RandomLottoGenerator.java
@@ -1,4 +1,20 @@
 package lotto.service;
 
-public class RandomLottoGenerator {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import lotto.Lotto;
+import lotto.domain.LottoNumber;
+
+public class RandomLottoGenerator implements LottoGenerator {
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+    @Override
+    public Lotto generate() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(
+                LottoNumber.MIN_NUMBER,
+                LottoNumber.MAX_NUMBER,
+                LOTTO_NUMBER_COUNT
+        );
+        return new Lotto(numbers); // Lotto 생성자가 정렬 및 검증
+    }
 }
