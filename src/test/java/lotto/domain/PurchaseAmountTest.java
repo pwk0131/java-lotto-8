0ce104ff92e9a5f60154fbd8692294ddc4dc457d
pwk0131 <pakr051323@gmail.com>
2025-10-31 21:03:03 +0900

    test(purchaseTest): PurchaseAmount 값 객체 테스트

---
+++ src/test/java/lotto/domain/PurchaseAmountTest.java
@@ -0,0 +1,44 @@
+package lotto.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import lotto.validation.ValidationMessages;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+@DisplayName("PurchaseAmount 값 객체 테스트")
+class PurchaseAmountTest {
+
+    @Test
+    @DisplayName("1000원 단위의 금액으로 객체를 생성할 수 있다")
+    void create_purchase_amount_success() {
+        int validAmount = 8000;
+
+        PurchaseAmount amount = new PurchaseAmount(validAmount);
+
+        assertThat(amount.getTicketCount()).isEqualTo(8);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1500, 2100, 8001})
+    @DisplayName("1000원 단위가 아닌 금액은 예외를 발생시킨다")
+    void create_fail_with_invalid_unit(int invalidAmount) {
+
+        assertThatThrownBy(() -> new PurchaseAmount(invalidAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ValidationMessages.ERROR_AMOUNT_INVALID_UNIT);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 500, -1000, 1})
+    @DisplayName("1000원 미만의 금액은 예외를 발생시킨다")
+    void create_fail_with_less_than_minimum(int invalidAmount) {
+
+        assertThatThrownBy(() -> new PurchaseAmount(invalidAmount))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ValidationMessages.ERROR_AMOUNT_MINIMUM);
+    }
+}
