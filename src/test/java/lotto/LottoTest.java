a911c66f69cb5befdbc2e1a6066b9e06b7bfe088
pwk0131 <pakr051323@gmail.com>
2025-10-31 23:38:34 +0900

    test(lotto): 로또 숫자 예외 테스트

--- src/test/java/lotto/LottoTest.java
+++ src/test/java/lotto/LottoTest.java
@@ -1,11 +1,13 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.List;
+import lotto.validation.ValidationMessages;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
     @Test
@@ -22,4 +24,29 @@ class LottoTest {
     }
 
     // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+
+    @DisplayName("로또 번호에 1 미만의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_1미만_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 0)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ValidationMessages.ERROR_LOTTO_NUMBER_OUT_OF_RANGE);
+    }
+
+    @DisplayName("로또 번호에 45 초과의 숫자가 있으면 예외가 발생한다.")
+    @Test
+    void 로또_번호에_45초과_숫자가_있으면_예외가_발생한다() {
+        assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 46)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(ValidationMessages.ERROR_LOTTO_NUMBER_OUT_OF_RANGE);
+    }
+
+    @DisplayName("Lotto 생성 시 번호가 오름차순으로 정렬된다.")
+    @Test
+    void 로또_번호가_생성시_오름차순으로_정렬된다() {
+        List<Integer> unsortedNumbers = List.of(6, 5, 4, 3, 2, 1);
+
+        Lotto lotto = new Lotto(unsortedNumbers);
+        assertThat(lotto.toString()).isEqualTo("[1, 2, 3, 4, 5, 6]");
+    }
 }
