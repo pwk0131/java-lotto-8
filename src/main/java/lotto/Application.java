94f53a4e88598b766ac216b85e8c32a5be248894
pwk0131 <pakr051323@gmail.com>
2025-10-31 23:10:27 +0900

    feat(Application): 로또 생성기능 시작점 연결

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,6 +1,8 @@
 package lotto;
 
 import lotto.controller.LottoGameController;
+import lotto.service.LottoGenerator;
+import lotto.service.RandomLottoGenerator;
 import lotto.view.InputView;
 import lotto.view.OutputView;
 
@@ -8,12 +10,14 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        LottoGenerator lottoGenerator = new RandomLottoGenerator();
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
 
         LottoGameController lottoGameController = new LottoGameController(
                 inputView,
-                outputView
+                outputView,
+                lottoGenerator
         );
         lottoGameController.run();
 
