e3a8ee33d43cbea6255a0cbf53d1089ee0fb932c
pwk0131 <pakr051323@gmail.com>
2025-11-01 19:23:54 +0900

    feat(outputView): 통계 및 수익률 출력

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,8 +1,14 @@
 package lotto.view;
 
+import java.text.NumberFormat;
+import java.util.Locale;
 import lotto.domain.Lottos;
+import lotto.domain.Rank;
+import lotto.domain.Statistics;
 
 public class OutputView {
+
+    private static final NumberFormat NUMBER_FORMAT = NumberFormat.getInstance(Locale.KOREA);
 
     public void printError(IllegalArgumentException e) {
         System.out.println(e.getMessage());
@@ -14,6 +20,41 @@ public class OutputView {
 
     public void printLottos(Lottos lottos) {
         System.out.println(lottos.toString());
+    }
+
+    public void printStatisticsHeader() {
+        System.out.println("\n당첨 통계");
+        System.out.println("---");
+    }
+
+    public void printStatistics(Statistics statistics) {
+        printRankResult(statistics, Rank.FIFTH);
+        printRankResult(statistics, Rank.FOURTH);
+        printRankResult(statistics, Rank.THIRD);
+        printRankResult(statistics, Rank.SECOND);
+        printRankResult(statistics, Rank.FIRST);
+    }
+
+    private void printRankResult(Statistics statistics, Rank rank) {
+        String description = formatRankDescription(rank);
+        String prize = formatPrize(rank.getPrize());
+        int count = statistics.getCount(rank);
+        System.out.printf("%s (%s원) - %d개\n", description, prize, count);
+    }
+
+    private String formatRankDescription(Rank rank) {
+        if (rank.isBonusRank()) {
+            return String.format("%d개 일치, 보너스 볼 일치", rank.getMatchCount());
+        }
+        return String.format("%d개 일치", rank.getMatchCount());
+    }
+
+    private String formatPrize(long prize) {
+        return NUMBER_FORMAT.format(prize);
+    }
+
+    public void printProfitRate(double rate) {
+        System.out.printf("총 수익률은 %,.1f%%입니다.\n", rate);
     }
 
 }
