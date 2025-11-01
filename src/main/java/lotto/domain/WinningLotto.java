784e1953da69ea4ea4503541e4431a043c8672a5
pwk0131 <pakr051323@gmail.com>
2025-11-01 19:16:20 +0900

    feat(lotto): 등수 판별 로직 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -18,5 +18,12 @@ public class WinningLotto {
             throw new IllegalArgumentException(ValidationMessages.ERROR_BONUS_DUPLICATE);
         }
     }
-    
+
+    public Rank determineRank(Lotto userLotto) {
+        int matchCount = userLotto.countMatchingNumbers(winningNumbers);
+        boolean hasBonus = userLotto.containsNumber(bonusNumber);
+
+        return Rank.valueOf(matchCount, hasBonus);
+    }
+
 }
