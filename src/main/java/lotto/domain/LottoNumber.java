979caf8e0df6a0f91be4984193b303f66145a3c7
pwk0131 <pakr051323@gmail.com>
2025-11-01 18:01:22 +0900

    feat(validator): 보너스 번호 원시값 포장

--- src/main/java/lotto/domain/LottoNumber.java
+++ src/main/java/lotto/domain/LottoNumber.java
@@ -1,6 +1,45 @@
 package lotto.domain;
 
+import java.util.Objects;
+import lotto.validation.ValidationMessages;
+
 public class LottoNumber {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 45;
+
+    private final int value;
+
+    public LottoNumber(int value) {
+        validateRange(value);
+        this.value = value;
+    }
+
+    private void validateRange(int value) {
+        if (value < MIN_NUMBER || value > MAX_NUMBER) {
+            throw new IllegalArgumentException(ValidationMessages.ERROR_LOTTO_NUMBER_OUT_OF_RANGE);
+        }
+    }
+
+    // 'getter' 대신 'getValue'를 사용 (의도를 명확히).
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LottoNumber that = (LottoNumber) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
 }
