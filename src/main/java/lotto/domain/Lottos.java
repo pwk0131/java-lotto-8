d4b75f6ebe8ffd34c45c4dee8a06dbedaf126442
pwk0131 <pakr051323@gmail.com>
2025-11-01 19:20:49 +0900

    feat(lottos): Lottos의 전체 통계를 계산하는 로직 추가

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -1,6 +1,9 @@
 package lotto.domain;
 
+import java.util.Collections;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import lotto.Lotto;
 
 public class Lottos {
@@ -8,6 +11,25 @@ public class Lottos {
 
     public Lottos(List<Lotto> lottos) {
         this.lottos = lottos;
+    }
+
+    public Statistics calculateStatistics(WinningLotto winningLotto) {
+        Map<Rank, Integer> result = createEmptyStatisticsMap();
+
+        for (Lotto lotto : lottos) {
+            Rank rank = winningLotto.determineRank(lotto); // 당첨 판별 로직 위임
+            result.put(rank, result.get(rank) + 1);
+        }
+
+        return new Statistics(Collections.unmodifiableMap(result));
+    }
+
+    private Map<Rank, Integer> createEmptyStatisticsMap() {
+        Map<Rank, Integer> result = new EnumMap<>(Rank.class);
+        for (Rank rank : Rank.values()) {
+            result.put(rank, 0);
+        }
+        return result;
     }
 
     // OutputView에서 출력을 담당할 toString
