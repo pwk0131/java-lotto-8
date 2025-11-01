96fb457493dc19bf979aa048337fe72a87d7eb1f
pwk0131 <pakr051323@gmail.com>
2025-11-01 19:30:30 +0900

    test(lottoTest): countMatchingNumbers 메서드 테스트

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -22,4 +22,27 @@ class LottosTest {
         String expected = "[1, 2, 3, 4, 5, 6]\n[7, 8, 9, 10, 11, 12]";
         assertThat(result).isEqualTo(expected);
     }
+
+    @Test
+    @DisplayName("당첨 통계를 정확히 계산한다")
+    void calculateStatistics_test() {
+        // 당첨번호: 1,2,3,4,5,6 / 보너스: 7
+        WinningLotto winningLotto = new WinningLotto(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)),
+                new LottoNumber(7)
+        );
+
+        Lotto lotto1 = new Lotto(List.of(1, 2, 3, 10, 11, 12)); // 5등
+        Lotto lotto2 = new Lotto(List.of(1, 2, 3, 4, 11, 12)); // 4등
+        Lotto lotto3 = new Lotto(List.of(1, 2, 10, 11, 12, 13)); // 꽝
+
+        Lottos lottos = new Lottos(List.of(lotto1, lotto2, lotto3));
+
+        Statistics statistics = lottos.calculateStatistics(winningLotto);
+
+        assertThat(statistics.getCount(Rank.FIFTH)).isEqualTo(1);
+        assertThat(statistics.getCount(Rank.FOURTH)).isEqualTo(1);
+        assertThat(statistics.getCount(Rank.MISS)).isEqualTo(1);
+        assertThat(statistics.getCount(Rank.FIRST)).isEqualTo(0);
+    }
 }
