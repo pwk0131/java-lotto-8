3cf6fae07ebc938eec65ccdad592984b8cfd5223
pwk0131 <pakr051323@gmail.com>
2025-11-01 22:07:59 +0900

    refactor(Statistics): Tell, Don't Ask 준수

--- src/main/java/lotto/domain/Statistics.java
+++ src/main/java/lotto/domain/Statistics.java
@@ -1,6 +1,10 @@
 package lotto.domain;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import lotto.dto.RankResultDTO;
+import lotto.dto.StatisticsReportDTO;
 
 public class Statistics {
     private final Map<Rank, Integer> results;
@@ -17,5 +21,27 @@ public class Statistics {
 
     public int getCount(Rank rank) {
         return results.getOrDefault(rank, 0);
+    }
+
+    public StatisticsReportDTO generateReport(PurchaseAmount purchaseAmount) {
+        List<RankResultDTO> rankResults = new ArrayList<>();
+
+        // View에 표시할 순서 (5등 -> 1등)
+        for (Rank rank : List.of(Rank.FIFTH, Rank.FOURTH, Rank.THIRD, Rank.SECOND, Rank.FIRST)) {
+            rankResults.add(createRankResultDTO(rank));
+        }
+
+        long totalPrize = calculateTotalPrize();
+        double profitRate = purchaseAmount.calculateProfitRate(totalPrize);
+
+        return new StatisticsReportDTO(rankResults, profitRate);
+    }
+
+    //DTO의 구성 요소인 RankResultDTO를 생성
+    private RankResultDTO createRankResultDTO(Rank rank) {
+        String description = rank.getDescription();
+        String prize = rank.getFormattedPrize();
+        int count = getCount(rank);
+        return new RankResultDTO(description, prize, count);
     }
 }
