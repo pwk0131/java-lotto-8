03f845995437a7e7ecd02ab5dbb2ffe1fed33a7b
pwk0131 <pakr051323@gmail.com>
2025-11-01 18:05:55 +0900

    feat(winningLotto): 당첨번호와 보너스 번호를 하나의 객체로 묶어주는 로직 추가

--- src/main/java/lotto/domain/WinningLotto.java
+++ src/main/java/lotto/domain/WinningLotto.java
@@ -1,4 +1,22 @@
 package lotto.domain;
 
+import lotto.Lotto;
+import lotto.validation.ValidationMessages;
+
 public class WinningLotto {
+    private final Lotto winningNumbers;
+    private final LottoNumber bonusNumber;
+
+    public WinningLotto(Lotto winningNumbers, LottoNumber bonusNumber) {
+        validate(winningNumbers, bonusNumber);
+        this.winningNumbers = winningNumbers;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(Lotto winningNumbers, LottoNumber bonusNumber) {
+        if (winningNumbers.containsNumber(bonusNumber)) {
+            throw new IllegalArgumentException(ValidationMessages.ERROR_BONUS_DUPLICATE);
+        }
+    }
+    
 }
