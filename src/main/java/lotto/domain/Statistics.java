29ab5a5a59de80497aa1367e20d321dec4c50696
pwk0131 <pakr051323@gmail.com>
2025-11-01 19:13:35 +0900

    feat(statistics): 통계결과 포장 일급 컬렉션 추가

--- src/main/java/lotto/domain/Statistics.java
+++ src/main/java/lotto/domain/Statistics.java
@@ -1,4 +1,21 @@
 package lotto.domain;
 
+import java.util.Map;
+
 public class Statistics {
+    private final Map<Rank, Integer> results;
+
+    public Statistics(Map<Rank, Integer> results) {
+        this.results = results;
+    }
+
+    public long calculateTotalPrize() {
+        return results.entrySet().stream()
+                .mapToLong(entry -> entry.getKey().getPrize() * entry.getValue())
+                .sum();
+    }
+
+    public int getCount(Rank rank) {
+        return results.getOrDefault(rank, 0);
+    }
 }
