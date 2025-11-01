7cbd35184b0ca655e36a6cd697c15002bbf3c7e9
pwk0131 <pakr051323@gmail.com>
2025-11-01 18:11:37 +0900

    feat(controller): 보너스 번호 로직 컨트롤러 연결

--- src/main/java/lotto/controller/LottoGameController.java
+++ src/main/java/lotto/controller/LottoGameController.java
@@ -3,6 +3,7 @@ package lotto.controller;
 import java.util.ArrayList;
 import java.util.List;
 import lotto.Lotto;
+import lotto.domain.LottoNumber;
 import lotto.domain.Lottos;
 import lotto.domain.PurchaseAmount;
 import lotto.service.LottoGenerator;
@@ -31,6 +32,12 @@ public class LottoGameController {
 
         // 3. 당첨 번호 입력
         Lotto winningNumbers = readWinningNumbersWithRetry();
+
+        // 4. 보너스 번호 입력
+        LottoNumber bonusNumber = readBonusNumberWithRetry(winningNumbers);
+
+        // 5. 당첨 로또 생성
+        WinningLotto winningLotto = new WinningLotto(winningNumbers, bonusNumber);
     }
 
     // 구입 금액 입력
@@ -78,5 +85,31 @@ public class LottoGameController {
         List<Integer> numbers = Validator.parseWinningNumbers(input);
         return new Lotto(numbers); // new Lotto()가 1~45 범위 검증을 수행
     }
+
+    private LottoNumber readBonusNumberWithRetry(Lotto winningNumbers) {
+        try {
+            return tryReadBonusNumber(winningNumbers);
+        } catch (IllegalArgumentException e) {
+            outputView.printError(e);
+            return readBonusNumberWithRetry(winningNumbers);
+        }
+    }
+
+    private LottoNumber tryReadBonusNumber(Lotto winningNumbers) {
+        String input = inputView.readBonusNumbers();
+        int number = Validator.parseBonusNumber(input);
+        LottoNumber bonusNumber = new LottoNumber(number); // 1~45 범위 검증
+
+        validateBonusNumberDuplication(winningNumbers, bonusNumber);
+
+        return bonusNumber;
+    }
+
+    private void validateBonusNumberDuplication(Lotto winningNumbers, LottoNumber bonusNumber) {
+        if (winningNumbers.containsNumber(bonusNumber)) {
+            throw new IllegalArgumentException(Validator.ERROR_BONUS_DUPLICATE);
+        }
+    }
+
 
 }
